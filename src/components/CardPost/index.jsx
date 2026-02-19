e88a27505d6746344c34e310933a4114394aa483
luispieta <lufelipepieta@gmail.com>
2026-02-19 19:51:19 -0300

    Primeiro commit: Projeto de api

---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,43 @@
+import styles from './cardpost.module.css'
+
+import { Author } from "../Author"
+import { ThumbsUpButton } from "./ThumbsUpButton"
+import { ModalComment } from "../ModalComment"
+import { Link } from "react-router"
+
+export const CardPost = ({ post }) => {
+    return (
+        <article className={styles.card}>
+            <header className={styles.header}>
+                <figure className={styles.figure}>
+                    <img
+                        src={post.cover}
+                        alt={`Capa do post de titulo: ${post.title}`}
+                    />
+                </figure>
+            </header>
+            <section className={styles.body}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <Link to={`/blog-post/${post.slug}`}>Ver detalhes</Link>
+            </section>
+            <footer className={styles.footer}>
+                <div className={styles.actions}>
+                    <div className={styles.action}>
+                        <ThumbsUpButton loading={false} />
+                        <p>
+                            {post.likes}
+                        </p>
+                    </div>
+                    <div className={styles.action}>
+                        <ModalComment />
+                        <p>
+                            {post.comments.length}
+                        </p>
+                    </div>
+                </div>
+                <Author author={post.author} />
+            </footer>
+        </article>
+    )
+}
